2bb7cca2155933c1d27a684abad7628645cb8bd2
atomosdovini <vini.felpe@gmail.com>
2022-03-17 00:32:45 -0300

    lts build vsr

--- src/containers/researchs/researchs.js
+++ src/containers/researchs/researchs.js
@@ -12,7 +12,7 @@
 //       active: true 
 //     });
 //     const [researchList, setResearchList] = useState([]);
-//     const baseURL = "https://consense-djangoapi.herokuapp.com/research/";
+//     const baseURL = "https://consense-djangoapi.herokuapp.com//research/";
 
 //     const deleteResearch = id =>{
 //       // delete research, depois apagar esta linha de baixo
