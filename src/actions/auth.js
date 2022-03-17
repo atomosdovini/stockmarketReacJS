9dae063990ba0e07dea5e1707d212a008bb023b9
atomosdovini <vini.felpe@gmail.com>
2022-03-17 01:29:41 -0300

    /auth test heroku

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -28,7 +28,7 @@ export const login = (email, password) => async dispatch => {
     const body = JSON.stringify({ email, password });
 
     try {
-        const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/jwt/create/`, body, config);
+        const res = await axios.post(`${process.env.REACT_APP_API_URL}auth/jwt/create/`, body, config);
         dispatch({
             type: LOGIN_SUCCESS,
             payload: res.data,
